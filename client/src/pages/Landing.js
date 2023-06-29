c5bf40e52f1ca8ba9d35e226bf738b0b6439b375
whyest <whyest@gmail.com>
2023-06-29 15:40:35 +0300

    added landing page

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,4 +1,31 @@
+import main from '../assets/images/main-alternative.svg'
+import Wrapper from '../assets/wrappers/LandingPage'
+import { Logo } from '../components'
+
 const Landing = () => {
-	return <div>Landing</div>;
-};
-export default Landing;
+  return (
+    <Wrapper>
+      <nav>
+        <Logo />
+      </nav>
+      <div className='container page'>
+        {/* info */}
+        <div className='info'>
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            Freegan ascot craft beer af you probably haven't heard of them DSA
+            salvia cred hashtag meh artisan organic cray master cleanse.
+            Coloring book venmo 8-bit irony man braid mixtape vexillologist
+            biodiesel bespoke godard.
+          </p>
+          <button className='btn btn-hero'>Login/Register</button>
+        </div>
+        {/* img */}
+        <img src={main} alt='job hunt' className='img main-img' />
+      </div>
+    </Wrapper>
+  )
+}
+export default Landing
