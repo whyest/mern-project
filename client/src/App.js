14c147eb7150c790a3a12164cd6c3c9ece3fb4d6
whyest <whyest@gmail.com>
2023-07-01 10:25:33 +0300

    added routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,21 @@
 import Landing from './pages/Landing'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 
 function App() {
   return (
-    <div>
-      <Landing />
-    </div>
+    <BrowserRouter>
+      <nav>
+        <Link to='/'>Dashboard</Link>
+        <Link to='/register'>Register</Link>
+        <Link to='/landing'>Landing</Link>
+      </nav>
+      <Routes>
+        <Route path='/' element={<div>Dashboard</div>} />
+        <Route path='/register' element={<div>Register</div>} />
+        <Route path='/landing' element={<Landing />} />
+        <Route path='*' element={<div>Error</div>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
